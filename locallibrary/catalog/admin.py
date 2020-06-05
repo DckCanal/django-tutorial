77821c1e88d73c02de735dd553a672226868445e
Marco De Canal <marco.decanal@gmail.com>
2020-06-05 15:39:18 +0200

    Migliorata vista in admin per Istanza e Autore

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -8,15 +8,37 @@ admin.site.register(Genere)
 #admin.site.register(Autore)
 admin.site.register(Lingua)
 
+class LibroInline(admin.TabularInline):
+	model = Libro
+	extra = False
+
 class AutoreAdmin(admin.ModelAdmin):
 	list_display = ('cognome','nome','data_nascita','data_morte')
-	
+	fields = [('nome','cognome'),('data_nascita','data_morte')]
+	inlines = [LibroInline]
 admin.site.register(Autore,AutoreAdmin)
+
+class IstanzaInline(admin.TabularInline):
+	model = Istanza
+	extra = False
+
+
 
 class LibroAdmin(admin.ModelAdmin):
 	list_display = ('titolo','autore','mostra_genere')
+	inlines = [IstanzaInline]
 admin.site.register(Libro, LibroAdmin)
 
 class IstanzaAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('libro','stato','riconsegna','id')
+	list_filter = ('stato','riconsegna')
+	exclude = ['id']
+	fieldsets = (
+		(None, {
+			'fields': ('libro','imprint')
+		}),
+		('Disponibilità',{
+			'fields': ('stato','riconsegna')
+		})
+	)
 admin.site.register(Istanza, IstanzaAdmin)
