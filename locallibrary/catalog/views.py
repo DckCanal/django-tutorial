2444c9cd7600702e51348160da3c6a37890c3477
Marco De Canal <marco.decanal@gmail.com>
2020-06-09 17:46:49 +0200

    Quasi terminata list-view dei libri

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Autore,Libro,Istanza,Genere
 from django.views import generic
+from .models import Autore, Libro, Istanza, Genere
 
 # Create your views here.
 
@@ -16,11 +16,12 @@ def index(request):
 
     n_genere_an = Genere.objects.filter(nome__icontains='an').count()
 
-    n_libri_la = Libro.objects.filter(titolo__contains='La').count()#è comunque case-insensitive...???
+    n_libri_la = Libro.objects.filter(titolo__contains='La').count()
+    #è comunque case-insensitive...???
 
 
 
-    c = {
+    cont = {
         'n_libri':n_libri,
         'n_istanze':n_istanze,
         'n_disponibili':n_ist_disp,
@@ -29,14 +30,15 @@ def index(request):
         'n_la':n_libri_la
     }
 
-    return render(request,'index.html',context = c)
+    return render(request, 'index.html', context=cont)
 
 
 class BookListView(generic.ListView):
+    """ListView per la classe models.Libro"""
     model = Libro
     context_object_name = 'elenco_libri'
     #queryset = Libro.objects.filter(titolo__contains='Il')
-    #template_name = 'pattern/template_personalizzato.html'
+    #template_name = 'catalog/templates/libro_list.html'
     #template di default: /locallibrary/catalog/templates/catalog/libro_list.html
 
     #def get_queryset(self):
@@ -44,7 +46,12 @@ class BookListView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         #Prima richiamo la funzione base
-        context = super(BookListView,self).get_context_data(**kwargs)
+        context = super(BookListView, self).get_context_data(**kwargs)
         #Poi aggiungo i dati che voglio io
-        context['VariabileCiullosa'] = 'Ciurillini' + 3
-        return context  
+        #context['VariabileCiullosa'] = 'Ciurillini'
+        return context
+
+class BookDetailView(generic.DetailView):
+    """DetailView per la classe models.Libro"""
+    model = Libro
+    #template_name = 'templates/dettaglio_libro.html'
