ccde08a78c1585fdb5fd116b81d71449905bf1db
marco.decanal <marco.decanal@gmail.com>
2020-06-13 16:54:57 +0200

    Session e creati 4 utenti in 2 gruppi

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,7 @@ urlpatterns = [
     path('books/',views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/',views.AutoreListview.as_view(), name = 'authors'),
-    path('author/<int:pk>',views.AutoreDetailView.as_view(),name='author-detail'),
+    #path('author/<int:pk>',views.AutoreDetailView.as_view(),name='author-detail'),
+    re_path(r'^author/(?P<pk>\d+)$',views.AutoreDetailView.as_view(), name='author-detail'),
+    #path('preferito/<int:pk>',views.setPreferito.as_view(),name='pref'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -4,6 +4,7 @@ from .models import Autore, Libro, Istanza, Genere
 
 # Create your views here.
 
+
 def index(request):
     """Vista per la pagina index."""
 
@@ -12,53 +13,70 @@ def index(request):
 
     n_ist_disp = Istanza.objects.filter(stato__exact='d').count()
 
-    n_autori = Autore.objects.count() #all() può essere sottinteso
+    n_autori = Autore.objects.count()  # all() può essere sottinteso
 
     n_genere_an = Genere.objects.filter(nome__icontains='an').count()
 
     n_libri_la = Libro.objects.filter(titolo__contains='La').count()
-    #è comunque case-insensitive...???
+    # è comunque case-insensitive...???
+    pref = request.session.get('libro_preferito', None)
+#    if pref:
+#        libro_preferito = Libro.objects.filter(id__exact=pref)
+#    else:
+#        libro_preferito = None
 
-
+    num_visit = request.session.get('num_visit', 0)
+    request.session['num_visit'] = num_visit+1
 
     cont = {
-        'n_libri':n_libri,
-        'n_istanze':n_istanze,
-        'n_disponibili':n_ist_disp,
-        'n_autori':n_autori,
-        'n_an':n_genere_an,
-        'n_la':n_libri_la
+        'n_libri': n_libri,
+        'n_istanze': n_istanze,
+        'n_disponibili': n_ist_disp,
+        'n_autori': n_autori,
+        'n_an': n_genere_an,
+        'n_la': n_libri_la,
+        'num_visit': num_visit,
+        # 'libro_preferito': libro_preferito,
     }
 
     return render(request, 'index.html', context=cont)
+
+# class setPreferito(generic.DetailView):
+#    model = Libro
+#    template_name = 'templates/catalog/set_pref.html'
 
 
 class BookListView(generic.ListView):
     """ListView per la classe models.Libro"""
     model = Libro
     context_object_name = 'elenco_libri'
-    paginate_by = 2
+    paginate_by = 5
     #queryset = Libro.objects.filter(titolo__contains='Il')
     #template_name = 'catalog/templates/libro_list.html'
-    #template di default: /locallibrary/catalog/templates/catalog/libro_list.html
+    # template di default: /locallibrary/catalog/templates/catalog/libro_list.html
 
-    #def get_queryset(self):
+    # def get_queryset(self):
     #   return Libro.object.all()[:5]
 
     def get_context_data(self, **kwargs):
-        #Prima richiamo la funzione base
+        # Prima richiamo la funzione base
         context = super(BookListView, self).get_context_data(**kwargs)
-        #Poi aggiungo i dati che voglio io
+        # Poi aggiungo i dati che voglio io
         #context['VariabileCiullosa'] = 'Ciurillini'
         return context
+
 
 class BookDetailView(generic.DetailView):
     """DetailView per la classe models.Libro"""
     model = Libro
     #template_name = 'templates/dettaglio_libro.html'
 
+
 class AutoreListview(generic.ListView):
     model = Autore
+    paginate_by = 5
+    context_object_name = 'elenco_autori'
+
 
 class AutoreDetailView(generic.DetailView):
     model = Autore
