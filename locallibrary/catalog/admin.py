3224d535654f7b099306cfcdc10bf3626ffcf5b9
Marco De Canal <marco.decanal@gmail.com>
2020-06-25 16:31:14 +0200

    Aggiunto borrower a Istanza

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,42 +3,52 @@ from catalog.models import Genere, Libro, Istanza, Autore, Lingua
 
 # Register your models here.
 admin.site.register(Genere)
-#admin.site.register(Libro)
-#admin.site.register(Istanza)
-#admin.site.register(Autore)
+# admin.site.register(Libro)
+# admin.site.register(Istanza)
+# admin.site.register(Autore)
 admin.site.register(Lingua)
 
+
 class LibroInline(admin.TabularInline):
-	model = Libro
-	extra = False
+    model = Libro
+    extra = False
+
 
 class AutoreAdmin(admin.ModelAdmin):
-	list_display = ('cognome','nome','data_nascita','data_morte')
-	fields = [('nome','cognome'),('data_nascita','data_morte')]
-	inlines = [LibroInline]
-admin.site.register(Autore,AutoreAdmin)
+    list_display = ('cognome', 'nome', 
+                    'data_nascita', 'data_morte')
+    fields = [('nome', 'cognome'), ('data_nascita', 'data_morte')]
+    inlines = [LibroInline]
+
+
+admin.site.register(Autore, AutoreAdmin)
+
 
 class IstanzaInline(admin.TabularInline):
-	model = Istanza
-	extra = False
-
+    model = Istanza
+    extra = False
 
 
 class LibroAdmin(admin.ModelAdmin):
-	list_display = ('titolo','autore','mostra_genere')
-	inlines = [IstanzaInline]
+    list_display = ('titolo', 'autore', 'mostra_genere')
+    inlines = [IstanzaInline]
+
+
 admin.site.register(Libro, LibroAdmin)
 
+
 class IstanzaAdmin(admin.ModelAdmin):
-	list_display = ('libro','stato','riconsegna','id')
-	list_filter = ('stato','riconsegna')
-	exclude = ['id']
-	fieldsets = (
-		(None, {
-			'fields': ('libro','imprint')
-		}),
-		('Disponibilità',{
-			'fields': ('stato','riconsegna')
-		})
-	)
+    list_display = ('libro', 'stato', 'borrower','riconsegna', 'id')
+    list_filter = ('stato', 'riconsegna')
+    exclude = ['id']
+    fieldsets = (
+        (None, {
+            'fields': ('libro', 'imprint')
+        }),
+        ('Disponibilità', {
+            'fields': ('stato', 'riconsegna', 'borrower')
+        })
+    )
+
+
 admin.site.register(Istanza, IstanzaAdmin)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,99 +1,123 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.admin import User
+from datetime import date
 
 # Create your models here.
 
+
 class Genere(models.Model):
-	"""Modello per il genere del libro."""
-	nome = models.CharField(max_length=200, help_text='Inserisci il genere (es: fantascienza)')
-	
-	def __str__(self):
-		"""String for representing the Model object."""
-		return self.nome
+    """Modello per il genere del libro."""
+    nome = models.CharField(
+        max_length=200, help_text='Inserisci il genere (es: fantascienza)')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.nome
+
 
 class Libro(models.Model):
-	"""Modello per rappresentare un libro (non una copia fisica)."""
-	titolo = models.CharField(max_length=200)
-	sommario = models.TextField(max_length=1000, help_text='Inserisci una breve descrizione del libro.')
-	isbn = models.CharField("ISBN",max_length=13, help_text='13 caratteri <a href="https://www.isbn-international.org/content/what-isbn"> ISBN </a>')
-	
-	genere = models.ManyToManyField(Genere, help_text="Scegli il genere per questo libro.")
-	autore = models.ForeignKey('Autore', on_delete = models.SET_NULL, null=True)
-	lingua = models.ForeignKey('Lingua', help_text = "Lingua", on_delete = models.SET_NULL, null=True)
-	
-	class Meta:
-		ordering = ['titolo']
-	def __str__(self):
-		"""String for representing the Model object."""
-		return self.titolo
-	
-	def get_absolute_url(self):
-		"""Return the url to access a detail record for this book."""
-		return reverse('book-detail',args=[str(self.id)])
-	
-	def mostra_genere(self):
-		"""Stringa per rappresentare i primi 3 generi, utile alla display_list dell'Admin"""
-		gens = []
-		for gen in self.genere.all()[:3]:
-			gens.append(gen.nome)
-		return ', '.join(gens)
-	
-	mostra_genere.short_description = 'Elenco generi'
-		
+    """Modello per rappresentare un libro (non una copia fisica)."""
+    titolo = models.CharField(max_length=200)
+    sommario = models.TextField(
+        max_length=1000, help_text='Inserisci una breve descrizione del libro.')
+    isbn = models.CharField(
+        "ISBN", max_length=13, help_text='13 caratteri <a href="https://www.isbn-international.org/content/what-isbn"> ISBN </a>')
+
+    genere = models.ManyToManyField(
+        Genere, help_text="Scegli il genere per questo libro.")
+    autore = models.ForeignKey('Autore', on_delete=models.SET_NULL, null=True)
+    lingua = models.ForeignKey(
+        'Lingua', help_text="Lingua", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ['titolo']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.titolo
+
+    def get_absolute_url(self):
+        """Return the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+    def mostra_genere(self):
+        """Stringa per rappresentare i primi 3 generi, utile alla display_list dell'Admin"""
+        gens = []
+        for gen in self.genere.all()[:3]:
+            gens.append(gen.nome)
+        return ', '.join(gens)
+
+    mostra_genere.short_description = 'Elenco generi'
+
+
 class Istanza(models.Model):
-	"""Modello che rappresenta una specifica copia di un libro, che può ad esempio essere prestata."""
-	id = models.UUIDField(primary_key=True,default=uuid.uuid4, help_text="ID univoco di uno specifico libro nella biblioteca.")
-	libro = models.ForeignKey(Libro, on_delete=models.SET_NULL, null=True)
-	imprint = models.CharField(max_length=200, help_text = "Non so cosa sia in effetti...")
-	riconsegna = models.DateField(null=True, blank=True)
-	
-	LOAN_STATUS = (
-		('m', 'Manutenzione'),
-		('p', 'Prestato'),
-		('d','Disponibile'),
-		('r','Riservato'),
-	)
-	
-	stato = models.CharField(
-		max_length = 1,
-		choices = LOAN_STATUS,
-		blank = True, 
-		default = 'm',
-		help_text = "Disponibilità del libro",
-	)
-	
-	class Meta:
-		ordering = ['riconsegna']
-	
-	def __str__(self):
-		"""String for representing the Model object."""
-		return f'{self.id} ({self.libro.titolo})'
-		#return f'{self.libro.titolo}'
+    """Modello che rappresenta una specifica copia di un libro, che può ad esempio essere prestata."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="ID univoco di uno specifico libro nella biblioteca.")
+    libro = models.ForeignKey(Libro, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(
+        max_length=200, help_text="Non so cosa sia in effetti...")
+    riconsegna = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Manutenzione'),
+        ('p', 'Prestato'),
+        ('d', 'Disponibile'),
+        ('r', 'Riservato'),
+    )
+
+    stato = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text="Disponibilità del libro",
+    )
+
+    class Meta:
+        ordering = ['riconsegna']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.libro.titolo})'
+        # return f'{self.libro.titolo}'
+
+    @property
+    def is_overdue(self):
+        if self.riconsegna and date.today() > self.riconsegna:
+            return True
+        return False
+    
+
 
 class Autore(models.Model):
-	"""Modello per rappresentare l'autore."""
-	nome = models.CharField(max_length=100)
-	cognome = models.CharField(max_length=100)
-	data_nascita = models.DateField(null=True, blank=True)
-	data_morte = models.DateField('Morto', null=True, blank=True)
-	
-	class Meta:
-		ordering = ['cognome','nome']
-	
-	def get_absolute_url(self):
-		"""Returns the url to access a particular author instance."""
-		return reverse('author-detail',args=[str(self.id)])
-	
-	def __str__(self):
-		"""String for representing the Model object."""
-		return f'{self.cognome}, {self.nome}'
-	
-class Lingua(models.Model):
-	"""Modello per rappresentare la lingua in cui è scritto un libro."""
-	nome = models.CharField(max_length=100, help_text="Inserisci il nome di una lingua, es. inglese, italiano...")
-	
-	def __str__(self):
-		"""String for representing the Model object."""
-		return self.nome
+    """Modello per rappresentare l'autore."""
+    nome = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100)
+    data_nascita = models.DateField(null=True, blank=True)
+    data_morte = models.DateField('Morto', null=True, blank=True)
 
+    class Meta:
+        ordering = ['cognome', 'nome']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.cognome}, {self.nome}'
+
+
+class Lingua(models.Model):
+    """Modello per rappresentare la lingua in cui è scritto un libro."""
+    nome = models.CharField(
+        max_length=100, help_text="Inserisci il nome di una lingua, es. inglese, italiano...")
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.nome
